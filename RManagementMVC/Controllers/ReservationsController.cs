34b449229782eb53a34909439d88bc3776ef835c
andersonandreas <adde1990@live.se>
2024-09-10 16:14:40 +0200

    Add Reservations feature with CRUD operations and views
    - Added a new `ReservationsController` to handle CRUD operations for reservations.
    - Introduced a new `Reservation` model class to represent reservation data.
    - Created a static `ReservationService` class to manage reservation data, including methods for getting, creating, updating, and deleting reservations.
    - Updated the `_Layout.cshtml` to replace the "Privacy" link with a "Reservations" link in the navigation bar and changed the footer text from "RManagementMVC" to "Restaurant MVC".
    - Modified the `Index.cshtml` for dishes to replace a button with a link for creating a new dish.
    - Added new views for reservations:
      * `Create.cshtml` for creating a new reservation.
      * `Edit.cshtml` for editing an existing reservation.
      * `Index.cshtml` for listing all reservations.
      * `_Reservation.cshtml` partial view for the reservation form used in create and edit views.
    - Added client-side validation and confirmation dialog for deleting reservations in the `Index.cshtml` view for reservations.

---
+++ RManagementMVC/Controllers/ReservationsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RManagementMVC.Models;
+using RManagementMVC.Services.Interfaces;
+
+namespace RManagementMVC.Controllers;
+
+public class ReservationsController : Controller
+{
+
+	private const string _createString = "create";
+	private const string _editString = "edit";
+
+
+	public IActionResult Index()
+	{
+		var reservations = ReservationService.GetAll();
+		return View(reservations);
+	}
+
+
+	public IActionResult Edit(string id)
+	{
+		ViewBag.Action = _editString;
+
+		var reservation = ReservationService.GetByID(Guid.Parse(id));
+		return View(reservation);
+	}
+
+
+	[HttpPost]
+	public IActionResult Edit(Reservation reservation)
+	{
+
+		if (ModelState.IsValid)
+		{
+			ReservationService.UpdateReservation(reservation);
+			return RedirectToAction(nameof(Index));
+		}
+
+		return View(reservation);
+	}
+
+
+	public IActionResult Create()
+	{
+		ViewBag.Action = _createString;
+
+		return View();
+	}
+
+
+	[HttpPost]
+	public IActionResult Create(Reservation reservation)
+	{
+		ViewBag.Action = _createString;
+
+		if (ModelState.IsValid)
+		{
+			ReservationService.Create(reservation);
+			return RedirectToAction(nameof(Index));
+		}
+
+		return View(reservation);
+	}
+
+
+	public IActionResult Delete(string id)
+	{
+		ReservationService.Delete(Guid.Parse(id));
+		return RedirectToAction(nameof(Index));
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RManagementMVC/Models/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace RManagementMVC.Models;
+
+public class Reservation
+{
+	public Guid Id { get; set; }
+	public string At { get; set; } = default!;
+	public string Name { get; set; } = default!;
+	public string? Email { get; set; }
+	public int Quantity { get; set; }
+}
