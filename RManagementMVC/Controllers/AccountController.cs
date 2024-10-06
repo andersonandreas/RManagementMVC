0a4b34fb012e7e7581692e27dd8c582b36768a8e
andersonandreas <adde1990@live.se>
2024-10-06 20:02:31 +0200

    add images

--- RManagementMVC/Controllers/AccountController.cs
+++ RManagementMVC/Controllers/AccountController.cs
@@ -7,50 +7,50 @@ namespace RManagementMVC.Controllers;
 public class AccountController(IAuthService authService, IUserService userService) : Controller
 {
 
-	private readonly IAuthService _authService = authService;
+    private readonly IAuthService _authService = authService;
 
 
 
 
-	public IActionResult Login()
-	{
-		if (userService.IsAdminLoggedIn())
-		{
-			return RedirectToAction("Panel", "Admin");
-		}
+    public IActionResult Login()
+    {
+        if (userService.IsAdminLoggedIn())
+        {
+            return RedirectToAction("Panel", "Admin");
+        }
 
-		return View();
-	}
-
-
-	[HttpPost]
-	public async Task<IActionResult> Login(LoginViewModel loginViewModel)
-	{
-
-		if (!ModelState.IsValid)
-		{
-			return View(loginViewModel);
-		}
-
-		var result = await _authService.LoginAsync(loginViewModel);
-
-		if (result.Success)
-		{
-			return RedirectToAction("Panel", "Admin");
-		}
-
-		ModelState.AddModelError(string.Empty, result.Message ?? "Invalid login attempt");
-
-		return View(loginViewModel);
-	}
+        return View();
+    }
 
 
-	public async Task<IActionResult> Logout()
-	{
-		await _authService.LogoutAsync();
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+    {
 
-		return View(nameof(Login));
-	}
+        if (!ModelState.IsValid)
+        {
+            return View(loginViewModel);
+        }
+
+        var result = await _authService.LoginAsync(loginViewModel);
+
+        if (result.Success)
+        {
+            return RedirectToAction("Panel", "Admin");
+        }
+
+        ModelState.AddModelError(string.Empty, result.Message ?? "Invalid login attempt");
+
+        return View(loginViewModel);
+    }
+
+
+    public async Task<IActionResult> Logout()
+    {
+        await _authService.LogoutAsync();
+
+        return RedirectToAction("Index", "Home");
+    }
 
 
 }
